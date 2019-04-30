f71e102c8ce7ee7a950d6bb426351e9eec08e0b6
thomaslnx <marcos.msilva10@gmail.com>
2019-04-30 11:19:46 -0300

    Completion of goBarber App

--- Desafio2/src/app/controllers/UserController.js
+++ Desafio2/src/app/controllers/UserController.js
@@ -1,6 +1,16 @@
+const { User } = require('../models')
+
 class UserController {
   create (req, res) {
     return res.render('auth/signup')
+  }
+
+  async store (req, res) {
+    const { filename: avatar } = req.file
+
+    await User.create({ ...req.body, avatar })
+
+    return res.redirect('/')
   }
 }
 
--- Desafio2/src/server.js
+++ Desafio2/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const session = require('express-session')
+const LokiStore = require('connect-loki')(session)
 const nunjucks = require('nunjucks')
 const path = require('path')
+const flash = require('connect-flash')
 
 class App {
   constructor () {
@@ -14,6 +17,18 @@ class App {
 
   middlewares () {
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(flash())
+    this.express.use(
+      session({
+        store: new LokiStore({
+          path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
+        }),
+        name: 'root',
+        secret: 'MyAppSecret',
+        resave: false,
+        saveUninitialized: true
+      })
+    )
   }
 
   views () {
